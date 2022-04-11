61d7d7a60af15b0a5298104e553c8b70ff9f22ba
Cameron <cc44@utas.edu.au>
2022-04-11 16:04:57 +1000

    added Section component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Hero from "../components/Hero";
 import MainLayout from "../layouts/MainLayout";
+import Section from "../layouts/Section";
 
 export default function Home() {
   return (
@@ -12,6 +13,14 @@ export default function Home() {
       }}
     >
       <Hero />
+      <Section color="primary">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+          eligendi dignissimos voluptates eius aliquam totam modi eum deserunt
+          facilis placeat impedit laborum rerum quam nemo nihil sed ipsum, velit
+          fuga?
+        </p>
+      </Section>
       <main className="main-body"></main>
     </MainLayout>
   );
